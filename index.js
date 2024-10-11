3dbc62a42b6d7bc16d9e33d3b7f7ef6a4b2f607c
Andreia <andreia.machion@saojudas.br>
2024-10-11 09:04:54 -0300

    finalizamos o backend

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const express = require('express')
 const app = express() //construindo uma aplicação express
 app.use(express.json())
 
+let filmes = [
+    {
+        titulo: "Dirty Dancing",
+        sinopse: "Na esperança de curtir sua juventude, Frances fica decepcionada ao descobrir que vai passar o verão de 1963 com os pais em um resort na sonolenta região de Catskills. A sua sorte muda quando ela conhece o instrutor de dança do resort, Johnny. Quando ele a coloca como sua nova parceira de dança, os dois acabam se apaixonando."
+    },
+    {
+        titulo: "Star Wars: Episódio III – A Vingança dos Sith",
+        sinopse: "As Guerras Clônicas estão em pleno andamento e Anakin Skywalker mantém um elo de lealdade com Palpatine, ao mesmo tempo em que luta para que seu casamento com Padmé Amidala não seja afetado por esta situação. Seduzido por promessas de poder, Anakin se aproxima cada vez mais de Darth Sidious até se tornar o temível Darth Vader. Juntos eles tramam um plano para aniquilar de uma vez por todas com os cavaleiros jedi."
+    }
+]
+
+
 //get url: http://localhost:3000/oi
 app.get ('/oi', (req, res) => {
     res.send('oi')
 })
 
+app.get ('/filmes', (req, res) => {
+    res.json(filmes)
+})
+
+app.post ('/filmes', (req, res) => {
+    //obter os dados que o cliente enviou
+    const titulo = req.body.titulo
+    const sinopse = req.body.sinopse
+    //montar o json filme
+    const filme = {titulo: titulo, sinopse: sinopse}
+    //inserir o filme na lista de filmes
+    filmes.push(filme)
+    res.json(filmes)
+})
 app.listen(3000, () => console.log("server up & running"))
